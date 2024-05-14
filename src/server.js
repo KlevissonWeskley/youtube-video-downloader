880ab56c36c372eb1e0a289193126447dfacd5e5
Klevisson Weskley <122038836+KlevissonWeskley@users.noreply.github.com>
2024-05-14 16:41:08 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import { createServer } from 'node:http'
 import path from 'node:path'
 import url from 'node:url'
 import routes from './routes.js'
+import cors from 'cors'
 
 const app = express()
 const server = createServer(app)
 
+app.use(cors)
 const currentPath = url.fileURLToPath(import.meta.url)
 const publicDir = path.join(currentPath, '../../', 'public')
 app.use(express.static(publicDir))
